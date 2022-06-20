cce4725567250b76e7e74332beb35e8dfcd1e2f2
stefanocravotta <stefano.cravotta8@gmail.com>
2022-06-20 15:47:28 +0200

    User

--- Class/User.php
+++ Class/User.php
@@ -3,10 +3,18 @@
 class User{
     private $name;
     private $surname;
+    private $scadenza_carta;
+    private $isIscritto = false;
+    private $discount = 0;
+    private $carta_scaduta = false;
 
-    public function __construct($_name, $_surname){
+    public function __construct($_name, $_surname , $_scadenza_carta , $_isIscritto){
         $this->name = $_name;
         $this->surname = $_surname;
+        $this->setScadenzaCarta($_scadenza_carta);
+        $this->isIscritto = $_isIscritto;
+        $this->setDiscount();
+        $this->setCartaScaduta($_scadenza_carta);
     }
 
     public function setName($_name){
@@ -17,12 +25,45 @@ class User{
         $this->surname = $_surname;
     }
 
+    public function setIsIscritto($_isIscritto){
+        $this->isIscritto = $_isIscritto;
+    }
+
+    public function setDiscount(){
+        if($this->isIscritto === true){
+            $this->discount = 20;
+        }
+    }
+
+    public function setScadenzaCarta($_scadenza_carta){
+        $this->scadenza_carta = date_create($_scadenza_carta);
+    }
+
+    public function setCartaScaduta($_scadenza_carta){
+        if( date_create($_scadenza_carta) < date_create('now')){
+            $this->carta_scaduta = true;
+        }
+    }
+
+
     public function getName(){
         return $this->name;
     }
 
     public function getSurname(){
         return $this->surname;
+    }
+
+    public function getIsIscritto(){
+        return $this->isIscritto;
+    }
+
+    public function getDiscount(){
+        return $this->discount;
+    }
+
+    public function getScadenzaCarta(){
+        return $this->scadenza_carta;
     }
 }
 
--- index.php
+++ index.php
@@ -5,12 +5,15 @@ require_once __DIR__ . '/Class/User.php';
 require_once __DIR__ . '/Class/Product_Class/Cibo.php';
 require_once __DIR__ . '/Class/Product_Class/Toy.php';
 require_once __DIR__ . '/Class/Product_Class/Cuccia.php';
+require_once __DIR__ . '/Class/User.php';
 
 $scatoletta = new Cibo('Pedigree',10 ,'pollo e carote');
 
 $pupazzo = new Toy('Trixie', 15 , 'gomma');
 
 $cuccia_grande = new Cuccia('Dreamaway', 129 , '100x150x45');
+
+$ugo = new User('Ugo', 'De Ughi' ,  "22-06-21" , false,);
 
 echo '<pre>';
 var_dump($scatoletta);
@@ -22,6 +25,10 @@ echo '</pre>';
 
 echo '<pre>';
 var_dump($cuccia_grande);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($ugo);
 echo '</pre>';
 
 ?>
